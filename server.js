9614ffdf3f2cc4051163b3a60b44a7c642cb3252
Timur Babyuk <timur.babyuk@gmail.com>
2013-04-01 11:59:50 +0300

    initial

---
+++ server.js
@@ -0,0 +1,9 @@
+﻿/// <reference path="./nodelib/node.js" />
+
+var path = require('path');
+var http = require('http');
+
+http.createServer(function (request, response) {
+    response.writeHead(200, { 'Content-Type': 'text/html' });
+    response.end('Take your echo, dude');
+}).listen(8080);
