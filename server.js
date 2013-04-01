43266c2b77e60b2fc3d21f08b04c728f853517ce
Timur Babyuk <timur.babyuk@gmail.com>
2013-04-01 13:57:55 +0300

    pushing changes

--- server.js
+++ server.js
@@ -5,5 +5,5 @@ var http = require('http');
 
 http.createServer(function (request, response) {
     response.writeHead(200, { 'Content-Type': 'text/html' });
-    response.end('Take your echo, dude! No reply pls');
+    response.end('Take your echo, dude! No reply pls!');
 }).listen(8080);
