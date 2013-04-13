a0d698700507c0c86632e3b579a7e9c0768ce7d1
Timur Babyuk <timur.babyuk@gmail.com>
2013-04-13 07:32:32 +0300

    socket.io added

--- server.js
+++ server.js
@@ -3,7 +3,27 @@
 var path = require('path');
 var http = require('http');
 
-http.createServer(function (request, response) {
+var app = http.createServer(function (request, response) {
     response.writeHead(200, { 'Content-Type': 'text/html' });
-    response.end('Take your echo, dude! No reply pls. V2');
-}).listen(process.env.PORT || 8080);
+    response.end('Take your echo');
+});
+
+var io = require('socket.io').listen(app);
+
+io.set('transports', [
+     'websocket'
+   , 'flashsocket'
+   , 'htmlfile'
+   , 'xhr-polling'
+   , 'jsonp-polling'
+]);
+
+var chat = io.of('/chat').on('connection', function (socket) {
+    console.log('OH MY~: Connected!');
+    socket.on('mess', function (data) {
+        console.log('OH MY~: Message!');
+        chat.emit('broad', data);
+    });
+});
+
+app.listen(process.env.PORT || 8080);
