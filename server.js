1807c6b0565ff9bfaec6659092c5ac9d13eeb157
Timur Babyuk <timur.babyuk@gmail.com>
2013-04-13 09:07:06 +0300

    trying to fix

--- server.js
+++ server.js
@@ -10,19 +10,29 @@ var app = http.createServer(function (request, response) {
 
 var io = require('socket.io').listen(app);
 
-io.set('transports', [
-     'websocket'
-   , 'flashsocket'
-   , 'htmlfile'
-   , 'xhr-polling'
-   , 'jsonp-polling'
-]);
+//io.set('transports', [
+//     'websocket'
+//   , 'flashsocket'
+//   , 'htmlfile'
+//   , 'xhr-polling'
+//   , 'jsonp-polling'
+//]);
 
-var chat = io.of('/chat').on('connection', function (socket) {
+//var chat = io.of('/chat').on('connection', function (socket) {
+//    console.log('OH MY~: Connected!');
+//    socket.on('mess', function (data) {
+//        console.log('OH MY~: Message!');
+//        chat.emit('broad', data);
+//    });
+//});
+
+//var chat =
+
+io.sockets.on('connection', function (socket) {
     console.log('OH MY~: Connected!');
     socket.on('mess', function (data) {
-        console.log('OH MY~: Message!');
-        chat.emit('broad', data);
+        console.log('OH MY~: Message! '+ data);
+        io.sockets.emit('broad', data);
     });
 });
 
